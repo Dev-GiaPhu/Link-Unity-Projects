0b7494c233555950dc907230de7e269924b17282
Dev-GiaPhu <jemnguyen114@gmail.com>
2025-10-16 22:42:27 +0700

    Auto commit 2025-10-16 22:42:27.847116

--- Clone.py
+++ Clone.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 # Thư mục chứa 2 file code Unity
-project_path = r"D:\FPT Polytechnic\NhapMonLTGA\Unity Projects" if sys.platform.startswith("win") else "/Users/username/UnityScripts"
+project_path = r"D:\FPT Polytechnic\NhapMonLTGA\Unity Projects" if sys.platform.startswith("win") else "/Users/khangng/Downloads/Hoc FPT/Unity project/Link Projects/Unity-Projects"
 os.chdir(project_path)
 
 # Git pull từ repository để lấy 2 file code mới nhất
--- Upload.py
+++ Upload.py
@@ -5,12 +5,12 @@ import sys
 
 # Thư mục chứa 2 file code Unity
 # Windows: r"D:\FPT Polytechnic\NhapMonLTGA\Unity Projects"
-# macOS: "/Users/username/UnityScripts"
-project_path = r"D:\FPT Polytechnic\NhapMonLTGA\Unity Projects" if sys.platform.startswith("win") else "/Users/username/UnityScripts"
+# macOS: "/Users/khangng/Downloads/Hoc FPT/Unity project/Link Projects/Unity-Projects"
+project_path = r"D:\FPT Polytechnic\NhapMonLTGA\Unity Projects" if sys.platform.startswith("win") else "/Users/khangng/Downloads/Hoc FPT/Unity project/Link Projects/Unity-Projects"
 os.chdir(project_path)
 
 # Git add 2 file code
-subprocess.run(["git", "add", "Script1.cs", "Script2.cs"])
+subprocess.run(["git", "add", "Upload.py", "Clone.py"])
 
 # Git commit với timestamp
 subprocess.run(["git", "commit", "-m", f"Auto commit {datetime.datetime.now()}"])
