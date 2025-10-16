52d1b0225c59c3ff257ae10ec81f115031ad7618
Dev-GiaPhu <jemnguyen114@gmail.com>
2025-10-16 23:50:29 +0700

    Auto commit 2025-10-16 23:50:29.596178

--- Upload.py
+++ Upload.py
@@ -5,8 +5,8 @@ import sys
 
 # Thư mục chứa 2 file code Unity
 # Windows: r"D:\FPT Polytechnic\NhapMonLTGA\Unity Projects"
-# macOS: "/Users/khangng/Downloads/Hoc FPT/Unity project/Link Projects/Unity-Projects"
-project_path = r"D:\FPT Polytechnic\NhapMonLTGA\Unity Projects" if sys.platform.startswith("win") else "/Users/khangng/Downloads/Hoc FPT/Unity project/Link Projects/Unity-Projects"
+# macOS: "/Users/khangng/Downloads/Hoc FPT/Unity project/Link-Unity-Projects"
+project_path = r"D:\FPT Polytechnic\NhapMonLTGA\Unity Projects" if sys.platform.startswith("win") else "/Users/khangng/Downloads/Hoc FPT/Unity project/Link-Unity-Projects"
 os.chdir(project_path)
 
 # Git add 2 file code
